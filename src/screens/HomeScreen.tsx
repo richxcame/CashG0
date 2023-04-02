71cfe492ed26dc0c120483e641a6ff60d41c5961
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2023-04-03 01:47:27 +0500

    feat: add infinite scroll for cashes

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,19 +1,100 @@
-import React from 'react';
-import {Button, Text, View} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {Button, View} from 'react-native';
+import {ChevronRightIcon} from 'native-base';
 
 import {styles} from '../styles';
 import {useAuth} from '../contexts/Auth';
+import {
+  Box,
+  FlatList,
+  HStack,
+  IconButton,
+  Spacer,
+  Text,
+  VStack,
+} from 'native-base';
+import useFetch from '../hooks/useFetch';
+
+export type CashBody = {
+  uuid: string;
+  amount: number;
+  client: string;
+  contact: string;
+  created_at: string;
+  detail: string;
+  note: string;
+};
+export type CashesResponse = {
+  cashes: CashBody[];
+  total: number;
+};
 
 export const HomeScreen = () => {
+  const [fetchURL, setFetchURL] = useState<string>('/cashes');
+  const [offset, setOffset] = useState<number>(0);
+  const [limit, _] = useState<number>(50);
+  const cashes = useRef<CashBody[]>([]);
+
   const auth = useAuth();
   const signOut = () => {
     auth.logout();
   };
 
+  const {data, error} = useFetch<CashesResponse>(fetchURL);
+  if (data && data.cashes) {
+    cashes.current = [...cashes.current, ...data.cashes];
+    // setCashes(old => [...old, ...data.cashes]);
+  }
+
+  // if (!data) {
+  //   return <Text>Loading...</Text>;
+  // }
+
+  if (error) {
+    return <Text>Couldn't fetch data</Text>;
+  }
+
+  const loadMore = () => {
+    setOffset(old => old + limit);
+    setFetchURL(`/cashes?offset=${offset + limit}&limit=${limit}`);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>HOME SCREEN</Text>
-      <Button title="Logout" onPress={signOut} />
-    </View>
+    <>
+      <Box>
+        <FlatList
+          data={cashes.current}
+          onEndReached={loadMore}
+          onEndReachedThreshold={0.3}
+          renderItem={({item}) => (
+            <Box borderColor="light.300" borderBottomWidth="0.3" px="2">
+              <HStack space={[2, 3]} justifyContent="space-between">
+                <VStack>
+                  <Text color="coolGray.800" bold>
+                    {item.contact} {item.amount}
+                  </Text>
+                  <Text color="coolGray.600">{item.client}</Text>
+                </VStack>
+                <Spacer />
+                <VStack justifyContent="center">
+                  <IconButton size="sm" icon={<ChevronRightIcon />} />
+                  {/* <Text
+                    fontSize="xs"
+                    color="coolGray.800"
+                    alignSelf="flex-start">
+                    {item.detail}
+                  </Text> */}
+                </VStack>
+              </HStack>
+            </Box>
+          )}
+          // keyExtractor={item => item.uuid}
+        />
+      </Box>
+      {!data && <Text>Loading...</Text>}
+      <View style={styles.container}>
+        <Button title="Logout" onPress={signOut} />
+      </View>
+    </>
   );
 };
