5ebffbfda65010e956ebe05333c8e7b803bafaab
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2023-04-05 19:08:17 +0500

    feat: add range screen

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {HomeScreen} from '../screens/HomeScreen';
+import {RangeScreen} from '../screens/RangeScreen';
+import {ReportScreen} from '../screens/ReportScreen';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export const AppStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Ranges" component={RangeScreen} />
+      <Tab.Screen name="Reports" component={ReportScreen} />
+    </Tab.Navigator>
   );
 };
