133bce9c0586d51a449faab8cf99c3728fd4517f
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2023-04-07 18:53:50 +0500

    feat: add ionic icons to bottom tabs

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {CashesScreen} from '../screens/CashesScreen';
 import {RangeScreen} from '../screens/RangeScreen';
 import {CashDetailsScreen} from '../screens/CashDetailsScreen';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from './RootNavigation';
 import {AboutScreen} from '../screens/AboutScreen';
 
@@ -23,12 +25,34 @@ export const AppStack = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen
-        options={{headerShown: false, title: 'Cashes'}}
+        options={{
+          headerShown: false,
+          title: 'Cashes',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="list-outline" color={color} size={size} />
+          ),
+        }}
         name="CashesStack"
         component={CashesStack}
       />
-      <Tab.Screen name="Ranges" component={RangeScreen} />
-      <Tab.Screen name="About" component={AboutScreen} />
+      <Tab.Screen
+        name="Ranges"
+        component={RangeScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="bar-chart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="About"
+        component={AboutScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="information-outline" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
